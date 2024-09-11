f87bff6cb7db0d9efd92bf3b78066a0e5bdb6510
abudhosamuel <abudhosamuel@gmail.com>
2024-09-11 14:44:57 +0300

    freebie tracker

--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,43 @@
 #!/usr/bin/env python3
 
 # Script goes here!
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Company(db.Model):
+    __tablename__ = 'companies'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    founding_year = db.Column(db.Integer)
+    
+    freebies = db.relationship('Freebie', backref='company')
+    
+    @property
+    def devs(self):
+        return {freebie.dev for freebie in self.freebies}
+
+class Dev(db.Model):
+    __tablename__ = 'devs'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    
+    freebies = db.relationship('Freebie', backref='dev')
+    
+    @property
+    def companies(self):
+        return {freebie.company for freebie in self.freebies}
+
+class Freebie(db.Model):
+    __tablename__ = 'freebies'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    item_name = db.Column(db.String, nullable=False)
+    value = db.Column(db.Integer, nullable=False)
+    dev_id = db.Column(db.Integer, db.ForeignKey('devs.id'))
+    company_id = db.Column(db.Integer, db.ForeignKey('companies.id'))
+
+    def print_details(self):
+        return f'{self.dev.name} owns a {self.item_name} from {self.company.name}'
